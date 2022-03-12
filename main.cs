d26f898aba901df175675fd2464ff9459a95de9f
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-12 04:57:50 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+// Created by: Marshall
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions of a rectangle are 5 cm and 3 cm");
+        Console.WriteLine("The area is " + (5 * 3));
+        Console.WriteLine("The perimeter is " + (2 * (5 + 3)));
+
+        Console.WriteLine("\nDone.");
+    }
+}
