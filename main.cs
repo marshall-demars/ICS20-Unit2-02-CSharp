eaccd125485abf9e714ae9e03d36d17ddcbc4e58
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-24 16:17:44 +0000

    cs file

--- main.cs
+++ main.cs
@@ -11,8 +11,8 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("The dimensions of a rectangle are 5 cm and 3 cm");
-        Console.WriteLine("The area is " + (5 * 3));
-        Console.WriteLine("The perimeter is " + (2 * (5 + 3)));
+        Console.WriteLine("The area is " + (5 * 3) + "cm²");
+        Console.WriteLine("The perimeter is " + (2 * (5 + 3)) + "cm");
 
         Console.WriteLine("\nDone.");
     }
